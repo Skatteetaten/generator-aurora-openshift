63177cbbec05e00e0a16547fa9832fb0c8b875b5
HaakonKlausen <hakon.klausen@skatteetaten.no>
2017-08-01 12:44:08 +0200

    Typo in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 which npm || (
   echo "Installing node" &&  \
-  sudo apt-get install npm node-legacy &&  \
+  sudo apt-get install npm nodejs-legacy &&  \
   npm install -g yo \
   sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share})
 
