c4fc5a99f405bfe7958d31ce630409273d3133e9
HaakonKlausen <hakon.klausen@skatteetaten.no>
2017-08-01 12:49:41 +0200

    added missing sudos in install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 which npm || (
   echo "Installing node" &&  \
   sudo apt-get install npm nodejs-legacy &&  \
-  npm install -g yo \
-  sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share})
+  sudo npm install -g yo \
+  sudo chown -R $(whoami) $(sudo npm config get prefix)/{lib/node_modules,bin,share})
 
-which yo || npm install -g yo
+which yo || sudo npm install -g yo
 
-npm install -g $DIR
+sudo npm install -g $DIR
 
 
