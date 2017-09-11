1c9764e602adb482de5d18bb049752b80f47e769
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-09-11 20:11:17 +0200

    updated openshift-deploy with pretty colors.

--- app/templates/files/openshift-deploy.sh
+++ app/templates/files/openshift-deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+INFO=$'\033[0;33m'
+CLEAR=$'\033[0m'
 
+header() {
+  printf "$INFO%s\n$CLEAR" "$*"
+}
 oc whoami -t &> /dev/null || echo "Log into oc and ao"
 
 yoRc=$(jq '.["generator-aurora-openshift"]' .yo-rc.json)
@@ -10,25 +15,27 @@ affiliation=$(echo $yoRc | jq ".affiliation" -r)
 
 namespace=$affiliation-$project
 
-oc get bc "$name" -n $namespace &> /dev/null || ao deploy $project/$name
+oc get bc "$name" -n $namespace &> /dev/null || (
+ header "AO deploy" && ao deploy $project/$name)
 
-echo "Mvn package"
+header "Mvn package"
 mvn package
 
 leveransepakke=$(find target -type f -name "*-Leveransepakke.zip")
 
-echo "Start OpenShift binary build"
+header "Start OpenShift binary build on OpenShift"
 oc start-build $name --from-file=$leveransepakke --follow --wait -n $namespace
 
 which stern &> /dev/null || exit 0
 
-echo "Tail logs with stern $name"
+header "Tail development $name"
 
 after_close() {
  url="http://$name-$namespace.utv.paas.skead.no/api/counter"
+ header "URL for application is"
  echo "$url"
 }
 
 trap after_close INT
 
-stern $name -n $namespace
+stern ${name} -n ${namespace}
